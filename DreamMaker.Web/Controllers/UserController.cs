fc5a001e7735df8dba4b439d31cf785cc5080e70
Brucezhang22 <895157882@qq.com>
2017-05-31 13:50:20 +0800

    更新
    更新

---
+++ DreamMaker.Web/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DreamMaker.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        /// <summary>
+        /// 登录用户自己的管理界面
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult MyWallet()
+        {
+            return View();
+        }
+    }
+}
