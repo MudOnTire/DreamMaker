6492e2a53a0efae7530b780ad66572090133e4dd
Brucezhang22 <895157882@qq.com>
2017-06-09 10:54:25 +0800

    更新
    更新

--- DreamMaker.Domain/Entities/IdentityModels.cs
+++ DreamMaker.Domain/Entities/IdentityModels.cs
@@ -8,12 +8,9 @@ namespace DreamMaker.Domain.Entities
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit http://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
-<<<<<<< HEAD
         /// <summary>
         /// 用户所属的房间
         /// </summary>
-=======
->>>>>>> f13c5d03debbbb094b49d0cf1f52b0da1e378f9d
         public virtual Room Room { get; set; }
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
